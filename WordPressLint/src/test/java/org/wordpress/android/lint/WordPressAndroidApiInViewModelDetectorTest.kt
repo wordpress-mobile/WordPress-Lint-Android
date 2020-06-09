a5fed049ff9a909afc2b4a27a3bd1488b0071461
Joel Dean <jdeanjj1000@gmail.com>
2020-06-09 02:13:32 -0500

    formatted code.

--- WordPressLint/src/test/java/org/wordpress/android/lint/WordPressAndroidApiInViewModelDetectorTest.kt
+++ WordPressLint/src/test/java/org/wordpress/android/lint/WordPressAndroidApiInViewModelDetectorTest.kt
@@ -1,6 +1,6 @@
 package org.wordpress.android.lint
 
-import com.android.tools.lint.checks.infrastructure.LintDetectorTest
+import com.android.tools.lint.checks.infrastructure.LintDetectorTest.kotlin
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
 import org.junit.Test
 import org.wordpress.android.lint.WordPressAndroidImportInViewModelDetector.Companion.ISSUE_ANDROID_IMPORT_IN_VIEWMODEL
@@ -8,9 +8,7 @@ import org.wordpress.android.lint.WordPressAndroidImportInViewModelDetector.Comp
 class WordPressAndroidApiInViewModelDetectorTest {
     @Test
     fun `when ViewModel contains Android import then test should fail`() {
-        lint().files(
-                LintDetectorTest.kotlin(
-                        """
+        lint().files(kotlin("""
             package test
             import android.widget.TextView
             import androidx.lifecycle.ViewModel
@@ -19,26 +17,27 @@ class WordPressAndroidApiInViewModelDetectorTest {
             class TestViewModel : ViewModel() {
               lateinit var textView: TextView
              }
-        """
-                ).indented())
+        """).indented())
                 .issues(ISSUE_ANDROID_IMPORT_IN_VIEWMODEL)
                 .run()
-                .expectErrorCount(1)
+                .expect("src/test/TestViewModel.kt:2: Error: ViewModels shouldn't " +
+                        "know anything about the Android framework classes. This improves " +
+                        "testability & modularity. [AndroidImportsInViewModel]\n" +
+                        "import android.widget.TextView\n" +
+                        "       ~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                        "1 errors, 0 warnings")
     }
 
     @Test
     fun `when ViewModel contains allowed Android R import then test should pass`() {
-        lint().files(
-                LintDetectorTest.kotlin(
-                        """
+        lint().files(kotlin("""
             package test
             import org.wordpress.android.fluxc.store.SiteStore
             import android.R.color.black
             import androidx.lifecycle.ViewModel
 
             class TestViewModel : ViewModel()
-        """
-                ).indented())
+        """).indented())
                 .issues(ISSUE_ANDROID_IMPORT_IN_VIEWMODEL)
                 .run()
                 .expectClean()
@@ -46,14 +45,11 @@ class WordPressAndroidApiInViewModelDetectorTest {
 
     @Test
     fun `ViewModel class should not trigger the lint check`() {
-        lint().files(
-                LintDetectorTest.kotlin(
-                        """
+        lint().files(kotlin("""
             package androidx.lifecycle
 
             class ViewModel
-        """
-                ).indented())
+        """).indented())
                 .issues(ISSUE_ANDROID_IMPORT_IN_VIEWMODEL)
                 .run()
                 .expectClean()
@@ -61,14 +57,11 @@ class WordPressAndroidApiInViewModelDetectorTest {
 
     @Test
     fun `A normal class shouldn't trigger the check`() {
-        lint().files(
-                LintDetectorTest.kotlin(
-                        """
+        lint().files(kotlin("""
             package test
 
             class TestClass
-        """
-                ).indented())
+        """).indented())
                 .issues(ISSUE_ANDROID_IMPORT_IN_VIEWMODEL)
                 .run()
                 .expectClean()
