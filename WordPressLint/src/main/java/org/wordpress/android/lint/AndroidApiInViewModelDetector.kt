ae1f1e88eacaca98cc960f2fc23c3cf625421abf
Joel Dean <jdeanjj1000@gmail.com>
2020-06-08 23:20:37 -0500

    created the base for the detector

---
+++ WordPressLint/src/main/java/org/wordpress/android/lint/AndroidApiInViewModelDetector.kt
@@ -0,0 +1,40 @@
+package org.wordpress.android.lint
+
+import com.android.tools.lint.client.api.UElementHandler
+import com.android.tools.lint.detector.api.*
+import org.jetbrains.uast.UImportStatement
+import java.util.*
+
+
+class AndroidApiInViewModelDetector: Detector(), Detector.UastScanner {
+    companion object {
+        @JvmStatic
+        val ISSUE_ANDROID_API_IN_VIEWMODEL =
+                Issue.create(
+                        id = "AndroidImportsInViewModel",
+                        briefDescription = "Disallows Android APIs from being used inside the ViewModel class.",
+                        explanation = "ViewModels shouldn't know anything about the Android framework classes" +
+                                ". This improves, testability & modularity.",
+                        category = Category.CORRECTNESS,
+                        priority = 5,
+                        severity = Severity.ERROR,
+                        implementation = Implementation(
+                                AndroidApiInViewModelDetector::class.java,
+                                EnumSet.of(Scope.JAVA_FILE)))
+
+    }
+
+    override fun getApplicableUastTypes() = listOf(UImportStatement::class.java)
+
+    override fun createUastHandler(context: JavaContext): UElementHandler? {
+        return object : UElementHandler() {
+            override fun visitImportStatement(node: UImportStatement) {
+                node.importReference?.let {
+
+                }
+            }
+        }
+    }
+}
+
+
