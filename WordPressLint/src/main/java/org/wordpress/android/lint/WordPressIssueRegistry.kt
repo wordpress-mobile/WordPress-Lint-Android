6aeb4fd440c73d74fe96f61a87cbcc722741747d
Matthew Kevins <mmkevins@yahoo.com>
2023-08-09 10:20:17 +1000

    Reformat registry code

--- WordPressLint/src/main/java/org/wordpress/android/lint/WordPressIssueRegistry.kt
+++ WordPressLint/src/main/java/org/wordpress/android/lint/WordPressIssueRegistry.kt
@@ -7,17 +7,20 @@ import com.android.tools.lint.detector.api.CURRENT_API
 class WordPressIssueRegistry : IssueRegistry() {
     override val api = CURRENT_API
     override val minApi = MIN_API
-    override val issues get() = listOf(
-            MissingNullAnnotationDetector.MISSING_FIELD_ANNOTATION,
-            MissingNullAnnotationDetector.MISSING_CONSTRUCTOR_PARAMETER_ANNOTATION,
-            MissingNullAnnotationDetector.MISSING_METHOD_PARAMETER_ANNOTATION,
-            MissingNullAnnotationDetector.MISSING_METHOD_RETURN_TYPE_ANNOTATION,
-    )
+    override val issues
+        get() = listOf(
+                MissingNullAnnotationDetector.MISSING_FIELD_ANNOTATION,
+                MissingNullAnnotationDetector.MISSING_CONSTRUCTOR_PARAMETER_ANNOTATION,
+                MissingNullAnnotationDetector.MISSING_METHOD_PARAMETER_ANNOTATION,
+                MissingNullAnnotationDetector.MISSING_METHOD_RETURN_TYPE_ANNOTATION,
+        )
+
     override val vendor = Vendor(
             vendorName = "WordPress Android",
             feedbackUrl = "https://github.com/wordpress-mobile/WordPress-Lint-Android/issues",
             identifier = "org.wordpress.android.lint",
     )
+
     companion object {
         const val MIN_API = 10
     }
