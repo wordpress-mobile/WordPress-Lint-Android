3bb5fda85123dfd1bc8c9088a327428d239ea490
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-07-01 14:31:49 +0200

    Introduce lint checks for mocking data and sealed classes

--- WordPressLint/src/main/java/org/wordpress/android/lint/WordPressIssueRegistry.kt
+++ WordPressLint/src/main/java/org/wordpress/android/lint/WordPressIssueRegistry.kt
@@ -3,22 +3,28 @@ package org.wordpress.android.lint
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.detector.api.CURRENT_API
+import slack.lint.mocking.DataClassMockDetector
+import slack.lint.mocking.SealedClassMockDetector
 
 class WordPressIssueRegistry : IssueRegistry() {
     override val api = CURRENT_API
     override val minApi = MIN_API
     override val issues
         get() = listOf(
-                MissingNullAnnotationDetector.MISSING_FIELD_ANNOTATION,
-                MissingNullAnnotationDetector.MISSING_CONSTRUCTOR_PARAMETER_ANNOTATION,
-                MissingNullAnnotationDetector.MISSING_METHOD_PARAMETER_ANNOTATION,
-                MissingNullAnnotationDetector.MISSING_METHOD_RETURN_TYPE_ANNOTATION,
+            MissingNullAnnotationDetector.MISSING_FIELD_ANNOTATION,
+            MissingNullAnnotationDetector.MISSING_CONSTRUCTOR_PARAMETER_ANNOTATION,
+            MissingNullAnnotationDetector.MISSING_METHOD_PARAMETER_ANNOTATION,
+            MissingNullAnnotationDetector.MISSING_METHOD_RETURN_TYPE_ANNOTATION,
+            DataClassMockDetector.issue.apply {
+                isEnabledByDefault()
+            },
+            SealedClassMockDetector.issue
         )
 
     override val vendor = Vendor(
-            vendorName = "WordPress Android",
-            feedbackUrl = "https://github.com/wordpress-mobile/WordPress-Lint-Android/issues",
-            identifier = "org.wordpress.android.lint",
+        vendorName = "WordPress Android",
+        feedbackUrl = "https://github.com/wordpress-mobile/WordPress-Lint-Android/issues",
+        identifier = "org.wordpress.android.lint",
     )
 
     companion object {
