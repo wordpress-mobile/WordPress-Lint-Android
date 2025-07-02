70aa0958fefb888614b3461f6fa7c9657e2f9d49
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-07-02 15:49:25 +0200

    Disable all Slack rules by default, except these about mocking data and sealed classes

--- WordPressLint/src/main/java/org/wordpress/android/lint/WordPressIssueRegistry.kt
+++ WordPressLint/src/main/java/org/wordpress/android/lint/WordPressIssueRegistry.kt
@@ -3,23 +3,36 @@ package org.wordpress.android.lint
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.detector.api.CURRENT_API
+import com.android.tools.lint.detector.api.Issue
+import slack.lint.SlackIssueRegistry
 import slack.lint.mocking.DataClassMockDetector
 import slack.lint.mocking.SealedClassMockDetector
 
 class WordPressIssueRegistry : IssueRegistry() {
+
+    private val slackIssueRegistry = SlackIssueRegistry()
+
     override val api = CURRENT_API
     override val minApi = MIN_API
-    override val issues
-        get() = listOf(
-            MissingNullAnnotationDetector.MISSING_FIELD_ANNOTATION,
-            MissingNullAnnotationDetector.MISSING_CONSTRUCTOR_PARAMETER_ANNOTATION,
-            MissingNullAnnotationDetector.MISSING_METHOD_PARAMETER_ANNOTATION,
-            MissingNullAnnotationDetector.MISSING_METHOD_RETURN_TYPE_ANNOTATION,
-            DataClassMockDetector.issue.apply {
-                isEnabledByDefault()
-            },
-            SealedClassMockDetector.issue
-        )
+    override val issues: List<Issue>
+        get() {
+            val allOwnIssues = listOf(
+                MissingNullAnnotationDetector.MISSING_FIELD_ANNOTATION,
+                MissingNullAnnotationDetector.MISSING_CONSTRUCTOR_PARAMETER_ANNOTATION,
+                MissingNullAnnotationDetector.MISSING_METHOD_PARAMETER_ANNOTATION,
+                MissingNullAnnotationDetector.MISSING_METHOD_RETURN_TYPE_ANNOTATION
+            )
+
+            val allSlackIssues = slackIssueRegistry.issues
+            val desiredSlackIssues = allSlackIssues.map { issue ->
+                val enable =
+                    issue.id == DataClassMockDetector.issue.id || issue.id == SealedClassMockDetector.issue.id
+
+                issue.setEnabledByDefault(enable)
+            }
+
+            return allOwnIssues + desiredSlackIssues
+        }
 
     override val vendor = Vendor(
         vendorName = "WordPress Android",
